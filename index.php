e0ffb50bf1a4e0fc75a3a5f09731537db5f812bf
mayammohasseli <maryam.mohasseli@gmail.com>
2017-03-14 19:24:51 +1100

    initial commit

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <script src="js/jquery-1.9.1.js"></script>
+    <script src="js/jquery.bxslider.js"></script>
+    <link rel="stylesheet" href="styles/jquery.bxslider.css">
+    <link rel="stylesheet" href="styles/mainTemplate.css" type="text/css">
+    <script>
+        $(document).ready(function(){
+            $("#slider").bxSlider({
+                auto:true,
+                pager:false
+            });
+        });
+
+    </script>
+</head>
+<body>
+<section class="top-panel">
+    <div class="container">
+        <ul class="panel-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Support</a></li>
+        </ul>
+        <ul class="social-list">
+            <li><a href="#"><img src="images/facebook.png" alt="Facebook" /></a> </li>
+            <li><a href="#"><img src="images/twitter.png" alt="Twitter" /></a> </li>
+            <li><a href="#"><img src="images/skype.png" alt="Skype" /></a> </li>
+            <li><a href="#"><img src="images/youtube.png" alt="YouTube" /></a> </li>
+            <li><a href="#"><img src="images/instagram.png" alt="Instagram" /></a> </li>
+            <li><a href="#"><img src="images/gmail.png" alt="Gmail" /></a> </li>
+            <li><a href="#"><img src="images/rssnews.png" alt="Rss News" /></a> </li>
+        </ul>
+    </div>
+</section>
+<header>
+    <div class="head container">
+        <h1><span class="firstSpan">Theme</span><span class="secondSpan"> Org</span></h1>
+
+        <form class="search">
+            <input type="text" name="search" placeholder="search"/>
+        </form>
+        <img src="images/search.png" />
+    </div>
+
+    <div class="menu">
+        <ul class="container second-header">
+            <li>
+                <a class="home" href="#"><img  src="images/home.png" alt="Home" />Home</a>
+
+            </li>
+            <li>
+                <a class="pages" href="#"><img  src="images/pages.png" alt="Pages"/>Pages</a>
+            </li>
+            <li>
+                <a class="features" href="#"><img  src="images/features.png" alt="Features"/>Features</a>
+            </li>
+            <li>
+                <a class="portfolio" href="#"><img  src="images/portfolio.png" alt="Portfolio"/>Portfolio</a>
+            </li>
+            <li>
+                <a class="presets" href="#"><img  src="images/presets.png" alt="Presets"/>Presets</a>
+            </li>
+            <li>
+                <a class="shortcodes" href="#"><img  src="images/shortcodes.png" alt="Shortcodes"/>Shortcodes</a>
+            </li>
+            <li>
+                <a href="#"><img  src="images/telephone.png" alt="Contact Us"/>Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</header>
+<!--slider start-->
+<ul id="slider">
+    <li class="design">
+        <div class="container">
+            <div>
+                <h1>Clean & Resposive Design</h1>
+                <h3>For multipurpose bussiness theme</h3>
+                <div class="paragraph">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu velit nisl.
+                        Proin venenatis iaculis ligula sit amet adipiscing. Morbi eu scelerisque nunc.
+                        Maecenas rhoncus porttitor magna, et malesuada justo cursus vel.
+                    </p>
+                </div>
+                <a class="responsive" href="#">Responsive Layout</a>
+                <a class="ultimate" href="#">Ultimate Color Variations</a>
+            </div>
+        </div>
+    </li>
+    <li class="slide5">
+        <div class="container">
+            <h1>Apply top most photo</h1>
+            <h2>Always be presentable</h2>
+            <div class="slide5-div">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim dui sit amet tellus
+                    vehicula eget lacinia est viverra.
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="slide2">
+        <div class="container">
+            <div class="slide2-h1"><h1>Clean & Resposive Design</h1></div>
+            <div class="slide2-h3"><h3>Responsive for all device</h3></div>
+            <h4>For multipurpose business theme</h4>
+        </div>
+    </li>
+    <li class="slide4">
+        <div class="container">
+            <h1>Play youtube video....!</h1>
+            <h2>Always be presentable</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dignissim dui sit amet tellus vehicula eget lacinia est viverra.</p>
+        </div>
+    </li>
+</ul>
+<!--slider finished-->
+
+
+
+<section class="purchase">
+    <div class="container">
+        <h3>Purchase a very clean, Responsive & Multi Purpose E-Tender Theme </h3>
+        <h4>Awesome Premium Sliders, Incredible Features, Unlimited Colors, Advance Theme Options ...</h4>
+        <a class="purchase-button" href="#">Purchase Now</a>
+    </div>
+</section>
+<section class="lectus container">
+    <div>
+        <ul class="lectus-image">
+            <li><img src="images/lectus1.jpg" /></li>
+            <li><img src="images/lectus2.jpg" /></li>
+            <li><img src="images/lectus3.jpg" /></li>
+            <li><img src="images/lectus4.jpg" /></li>
+        </ul>
+    </div>
+    <div>
+        <ul class="lectus-text">
+            <li>
+                <div class="text">
+                    <h3>lectus tincidunt imperd</h3>
+                    <p>Lorem ipsum dolor sit amet, ectetur adipiscing elit. Maecenas in dignissim nulla.</p>
+                </div>
+            </li>
+            <li>
+                <div class="text">
+                    <h3>lectus tincidunt imperd</h3>
+                    <p>Lorem ipsum dolor sit amet, ectetur adipiscing elit. Maecenas in dignissim nulla.</p>
+                </div>
+            </li>
+            <li>
+                <div class="text">
+                    <h3>lectus tincidunt imperd</h3>
+                    <p>Lorem ipsum dolor sit amet, ectetur adipiscing elit. Maecenas in dignissim nulla.</p>
+                </div>
+            </li>
+            <li>
+                <div class="text">
+                    <h3>lectus tincidunt imperd</h3>
+                    <p>Lorem ipsum dolor sit amet, ectetur adipiscing elit. Maecenas in dignissim nulla.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <img class="blue-line" src="images/blue-line.jpg" />
+
+</section>
+
+<section class="welcome container">
+    <ul class="middle">
+        <li class="e-tender">
+            <h3>Welcome to E-tender template</h3>
+            <div class="line1"><img class="welcome-image" src="images/welcome.jpg" /></div>
+            <div class="paragraph-template">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in dignissinulla. Quisque mollis sodales justo, quis adipiscing magna imperdiet nec. Curabitur sem arcu, pretium eget varius vel, hendrerit eu justo. Ut gravida mollis augue, a mollis nunc ultrices accumsan. Suspendisse eu velit leo. orem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in dignissinulla. Quisque mollis sodales justo, quis adipiscing magna imperdiet nec. Curabitur sem arcu, </div>
+
+            <a href="#"><img class="read" src="images/read.jpg" /></a>
+        </li>
+        <li class="newsletters">
+            <h3>Recent newsletters</h3>
+            <div class="first-div">
+                <h5>In ut urna ipsum, sit amet egestas magna.</h5>
+                <ul class="first-div-ul">
+                    <li><img src="images/soil1.jpg" /></li>
+                    <div class="detail1">
+                        <div><a href="#"><img src="images/calendar.png" />March 15, 2013</a></div>
+                        <div><a href="#"><img src="images/comment.png" />5 comments</a></div>
+                    </div>
+                    <li class="myFirstparagraph">
+                        Lorem ipsum dolor sit amet, sectetur adipiscing elit. Maecenas in dignissim nulla. Quisque mollis sodales justo, quis adipiscing magna.
+                    </li>
+                </ul>
+            </div>
+            <div class="second-div">
+                <h5>Sed id nulla et velit commodo feugiat sed vitae risus.</h5>
+                <ul class="second-div-ul">
+                    <li><img src="images/soil1.jpg" /></li>
+                    <div class="detail2">
+                        <div><a href="#"><img src="images/calendar.png" />March 15, 2013</a></div>
+                        <div><a href="#"><img src="images/comment.png" />5 comments</a></div>
+                    </div>
+                    <li class="mySecondparagraph">
+                        Lorem ipsum dolor sit amet, sectetur adipiscing elit. Maecenas in dignissim nulla. Quisque mollis sodales justo, quis adipiscing magna.
+                    </li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</section>
+
+<section class="testimonial container">
+    <div class="myTestimonial">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et laoreet arcu. Pellentesque gravida nisl nec sapien malesuada sed iaculis neque volutpat. Duis odio leo, posuere ac aliquam vitae, dignissim vitae orci. Phasellus velit nisi, tristique vitae convallis non, varius non dui</p>
+    </div>
+    <div class="marcla">
+        <span>Marcla leo</span>
+        <span>( CEO world bigest compay pvt. ltd. )</span>
+    </div>
+</section>
+<section class="ourPartner container">
+    <h3>Our partner</h3>
+    <ul class="last-list clearfix">
+        <li>
+            <a href="#"><img src="images/themeforest-icon.jpg" /></a>
+        </li>
+        <li>
+            <a href="#"><img src="images/envato-icon.jpg" /></a>
+        </li>
+        <li>
+            <a href="#"><img src="images/photodune-icon.jpg" /></a>
+        </li>
+        <li>
+            <a href="#"><img src="images/jungle-icon.jpg" /></a>
+        </li>
+    </ul>
+</section>
+<section class="twitter">
+    <div class="container">
+        <a href="#"><img src="images/twitter1.png" alt="twitter" />Ut consequat tortor sed massa commodo in ultrices tellus
+            scelerisque ultrices tellus scelerisquerice.
+            massa commodo in ultrices tellus scelerisque.
+        </a>
+    </div>
+</section>
+<footer>
+    <div class="footer container">
+        <ul class="footer-div">
+        <li class="link-div">
+            <ul class="link">
+                <h3>Useful Link</h3>
+                <li><p> >Sed consequainterd ullamcorper.</p></li>
+                <li><p> >Sed consequainterd ullamcorper.</p></li>
+                <li><p> >Sed consequainterd ullamcorper.</p></li>
+                <li><p> >Sed consequainterd ullamcorper.</p></li>
+                <li><p> >Sed consequainterd ullamcorper.</p></li>
+            </ul>
+        </li>
+        <li class="tags">
+            <h3>Popular Tags</h3>
+            <div>
+                <a class="clean" href="#">Clean</a>
+                <a class="template" href="#">Template</a>
+                <a class="clear" href="#">Clear</a>
+                <a class="business" href="#">Bussiness Template</a>
+                <a class="cool" href="#">Cool</a>
+                <a class="popular" href="#">Popular</a>
+                <a class="responsive-theme" href="#">Responsive Theme</a>
+                <a class="professional" href="#">Professional</a>
+            </div>
+        </li>
+        <li class="address">
+            <h3>Adress</h3>
+            <address>
+                <div>
+                <ul class="location">
+                    <li><img src="images/location-icon.png" /></li>
+                    <li>
+                        <ul class="second-location">
+                            <li>1110 Evergreen Lane,</li><br>
+                            <li>Los Angeles, CA 90017,</li><br>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="telephone">
+                    <li><img src="images/phone-icon.png" /></li>
+                    <li>
+                        <ul class="second-telephone">
+                            <li><a class="telephoneNumber" href="tel://+88123456789">+(088) 123 456 789</a></li><br>
+                            <li><a href="tel://+88123456789">+(088) 123 456 789</a></li><br>
+                        </ul>
+                    </li>
+                </ul>
+                    <ul class="email">
+                    <li><img class="email" src="images/email-icon.png" /></li>
+                    <li><a href="mailto:info@email.com">info@email.com</a></li>
+                </ul>
+                </div>
+            </address>
+        </li>
+        <li class="about">
+            <h3>About Us</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consect-<br>etur adipiscing elit.
+                Curabitur semperdapibus nulla eu suscipit.
+                Ut pretium<br> scelerisque quam Ut pretium quam,<br> vel
+            </p>
+            <h4>Connect With Us</h4>
+            <ul class="social">
+                <li><img class="fb" src="images/facebook-icon.jpg" alt="facebook"/></li>
+                <li><img src="images/twitter-icon.jpg" alt="twitter"/></li>
+                <li><img src="images/pinterest-icon.jpg" alt="pinterest"/></li>
+                <li><img src="images/google-plus-icon.jpg" alt="google+"/></li>
+            </ul>
+        </li>
+        <div style="clear:both;"></div>
+        </ul>
+    </div>
+    <span class="copyright">&copy;copyright 2014, My Portfolio Design</span>
+</footer>
+
+</body>
+</html>
